114df1b81c8209f4752ab6a8ae5e9f68fb924161
kit <kit.mavareik@gmail.com>
2024-09-04 18:18:52 +0300

    Rename and refactor App component for improved modularity
    Moved `App.tsx` to `App/App.tsx` for better structure and reusability. Updated to use the new `Router` component and clean up unused route-related code. This change enhances the organization and maintainability of the project.

---
+++ src/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {CssBaseline} from "@mui/material";
+
+import {Router} from './utilitary/Router/Router'
+
+export const App = () => {
+  return (
+      <>
+          <CssBaseline/>
+          <Router/>
+      </>
+  );
+}
---
+++ src/App/index.ts
@@ -0,0 +1,4 @@
+export * from './utilitary/ThemeProvider/ThemeProvider'
+export * from './utilitary/ThemeProvider/ThemeProvider.hooks'
+export * from './utilitary/Router/Router'
+export * from './App'
