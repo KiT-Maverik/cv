babd0ff615e0a67b8d1624cdbcb703885b677c27
kit <kit.mavareik@gmail.com>
2024-09-04 20:38:54 +0300

    Add skills display with MUI components
    Integrated Material-UI Box, Chip, Stack, and Typography components to display a list of skills. Skills are now shown in a responsive and styled manner using data from the software-developer skills file.

--- src/design/organisms/Skills/Skills.tsx
+++ src/design/organisms/Skills/Skills.tsx
@@ -1,12 +1,20 @@
 import style from './Skills.styles'
+import {Box, Chip, Stack, Typography} from "@mui/material";
+import {skills} from "../../../data/software-developer/skills";
+import React from "react";
 
 interface SkillsProps {
 }
 
 export const Skills = ({}: SkillsProps) => {
     return (
-        <>
-
-        </>
+        <Box>
+            <Typography variant='h3'>
+                Skills
+            </Typography>
+            <Stack direction='row' flexWrap='wrap' gap={1}>
+                {skills.map(item => <Chip label={item} variant="outlined"/>)}
+            </Stack>
+        </Box>
     )
 }
