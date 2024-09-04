00706258ff8f3d6296214e0d96a9a69a32a18b9f
kit <kit.mavareik@gmail.com>
2024-09-04 18:45:28 +0300

    Refactor route constants and add ThemeProvider
    Centralize route definitions in a new file, simplifying import paths. Replace usage of outdated route constants in Router component. Integrate ThemeProvider into App component for consistent theming.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {CssBaseline} from "@mui/material";
 
+import {ThemeProvider} from "./utilitary/ThemeProvider/ThemeProvider";
 import {Router} from './utilitary/Router/Router'
 
 export const App = () => {
   return (
-      <>
+      <ThemeProvider>
           <CssBaseline/>
           <Router/>
-      </>
+      </ThemeProvider>
   );
 }
--- src/App/utilitary/Router/Router.tsx
+++ src/App/utilitary/Router/Router.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
-import {routes} from "api/constants/routes.constats";
+import {route} from "constants/routes.contants";
 import {HomePage} from "design/pages";
 import {Layout} from "design/templates";
 
@@ -9,7 +9,7 @@ export const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path={routes.root} element={<Layout />}>
+                <Route path={route.home} element={<Layout />}>
                     <Route index element={<HomePage />} />
                 </Route>
             </Routes>
