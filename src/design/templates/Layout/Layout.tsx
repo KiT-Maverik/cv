118c0698826c0e2f76456442a653c22acde52de1
kit <kit.mavareik@gmail.com>
2024-09-04 18:36:03 +0300

    Refactor Layout component to remove drawer functionality
    Removed the drawer and related code from the Layout component, including state management and API query calls for movies thumbnails. Simplified the component structure by focusing only on the essential elements for rendering the main content.

--- src/design/templates/Layout/Layout.tsx
+++ src/design/templates/Layout/Layout.tsx
@@ -3,73 +3,25 @@ import {
     AppBar,
     Box,
     Button,
-    Drawer,
     Stack,
-    Skeleton,
     Toolbar,
-    Typography,
-    ListItemText,
-    ListItemIcon, MenuItem, MenuList
 } from "@mui/material";
-import React, {useEffect, useMemo, useState} from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
-
-import {useGetMoviesThumbnailListQuery} from "api/queries/movies/getMoviesThumbnailList.query";
-import {routes} from "api/constants/routes.constats";
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 import style from './Layout.styles'
 
 export function Layout() {
-    const [showDrawer, setShowDrawer] = useState(false)
-
-    const navigate = useNavigate()
-    const {moviesThumbnailList, getMoviesThumbnailList, loading} = useGetMoviesThumbnailListQuery()
-
-    useEffect(() => getMoviesThumbnailList(), [])
-
-    const drawerContent = useMemo(() => {
-        if (loading || !moviesThumbnailList.length) return [1, 2, 3, 4, 5].map(item => (
-            <Box key={item} sx={style.drawer.item.container}>
-                <Skeleton sx={style.skeleton.cover}/>
-                <Skeleton sx={style.skeleton.label}/>
-            </Box>
-        ))
-        else return (
-            <MenuList>
-                {
-                    moviesThumbnailList.map(({title, year, thumb, id}) => (
-                        <MenuItem key={title} onClick={() => {
-                            navigate(routes.movie(id.toString()))
-                            setShowDrawer(false)
-                        }}>
-                            <ListItemIcon>
-                                <Box component='img' src={thumb} sx={style.drawer.item.cover}/>
-                            </ListItemIcon>
-                            <ListItemText>
-                                <Typography color='text.secondary'>{`${title} (${year})`}</Typography>
-                            </ListItemText>
-                        </MenuItem>
-                    ))
-                }
-            </MenuList>
-        )
-    }, [loading, moviesThumbnailList]);
-
     return (
-        <>
-            <Drawer open={showDrawer} onClose={() => setShowDrawer(false)}>
-                {drawerContent}
-            </Drawer>
             <Stack sx={style.container}>
                     <AppBar position="static">
                         <Toolbar>
-                            <Button color="inherit" startIcon={<MenuRoundedIcon />} onClick={() => setShowDrawer(true)}>Movie Catalog</Button>
+                            <Button color="inherit" startIcon={<MenuRoundedIcon />}>Movie Catalog</Button>
                         </Toolbar>
                     </AppBar>
                 <Box component='main' sx={style.main}>
                     <Outlet/>
                 </Box>
             </Stack>
-        </>
     );
 }
