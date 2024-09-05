61f7f108358978677f6b00f5fdbe66a65cf2963e
kit <kit.mavareik@gmail.com>
2024-09-05 21:28:59 +0300

    Refactor Experience component styles and structure
    Updated the Experience component structure by modularizing and enhancing styles for periods, descriptions, and overall layout. Improved semantic HTML usage and added stronger visual emphasis on key elements.

--- src/design/organisms/Experience/Experience.styles.ts
+++ src/design/organisms/Experience/Experience.styles.ts
@@ -10,30 +10,51 @@ const container: SxProps<Theme> = {
 
 const header: SxProps<Theme> = commonStyles.header
 
-const experience: { container: SxProps<Theme>; period: SxProps<Theme> } = {
+const experience: {
+    container: SxProps<Theme>;
+    divider: SxProps<Theme>;
+    period: { container: SxProps<Theme>; item: SxProps<Theme> }
+    description: {
+        responsibility: { list: SxProps<Theme>; item: SxProps<Theme> }
+    }
+} = {
     container: {
-        display: "flex",
-        gap: 3,
+        display: "grid",
+        gridTemplateColumns: "auto auto auto",
+        rowGap: 5,
+        columnGap: 5,
     },
     period: {
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-    }
-} as const
-
-const responsibility: { list: SxProps<Theme>; item: SxProps<Theme> } = {
-    list: {
-        pl: 5,
+        container: {
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            fontWeight: 700,
+            textTransform: "uppercase",
+        },
+        item: {
+          fontWeight: 700,
+        },
     },
-    item: {
-        listStyleType: 'disc',
+    divider: {
+        background: '#b37f56',
+    },
+    description: {
+        responsibility: {
+            list: {
+                pl: 5,
+                m: 0,
+            },
+            item: {
+                listStyleType: 'disc',
+                textAlign: 'justify'
+            }
+        }
     }
 } as const
 
 export default {
     container,
     header,
-    responsibility,
     experience,
 }
--- src/design/organisms/Experience/Experience.tsx
+++ src/design/organisms/Experience/Experience.tsx
@@ -11,27 +11,28 @@ export const Experience = () => {
     const {experience} = useData()
     const { locale } = useContext(AppSettings)
 
-
     return (
         <Box sx={style.container}>
             <Typography variant='h2' sx={style.header}><WorkIcon/>Work Experience</Typography>
-            {experience.map(({start, end, responsibilities, company, position}) => (
-                    <Box sx={style.experience.container}>
-                        <Stack sx={style.experience.period}>
-                            <Typography>{end}</Typography>
-                            <Typography>-</Typography>
-                            <Typography>{start}</Typography>
+            <Box sx={style.experience.container}>
+                {experience.map(({start, end, responsibilities, company, position}) => (
+                    <>
+                        <Stack sx={style.experience.period.container}>
+                            <Typography sx={style.experience.period.item}>{end}</Typography>
+                            <Typography sx={style.experience.period.item}>-</Typography>
+                            <Typography sx={style.experience.period.item}>{start}</Typography>
                         </Stack>
-                        <Divider orientation='vertical'/>
+                        <Divider orientation='vertical' sx={style.experience.divider}/>
                         <Box>
-                            <Typography>{`${company}, ${position}`}</Typography>
-                            <Box component='ul' sx={style.responsibility.list}>
-                                {responsibilities.map(item => <Typography component='li' sx={style.responsibility.item}>{item[locale]}</Typography>)}
+                            <Typography variant='h3' fontWeight={700}>{`${company} || ${position}`}</Typography>
+                            <Box component='ul' sx={style.experience.description.responsibility.list}>
+                                {responsibilities.map(item => <Typography component='li' sx={style.experience.description.responsibility.item}>{item[locale]}</Typography>)}
                             </Box>
                         </Box>
-                    </Box>
+                    </>
                 ))
-            }
+                }
+            </Box>
         </Box>
     )
 }
