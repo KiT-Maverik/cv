f335d6edde46774c8912c7707324c0f4b99c8fc3
kit <kit.mavareik@gmail.com>
2024-09-05 16:15:44 +0300

    Add locale toggle functionality to HomePage
    Introduced `useContext` to access `AppSettings` and implemented a function to toggle between EN and UA locales. This functionality is triggered via the existing Translate button on the HomePage.

--- src/design/pages/HomePage/HomePage.tsx
+++ src/design/pages/HomePage/HomePage.tsx
@@ -2,15 +2,23 @@ import PrintIcon from '@mui/icons-material/LocalPrintshop';
 import TranslateIcon from '@mui/icons-material/Translate';
 import TextIcon from '@mui/icons-material/Assignment';
 import {Box, Container, IconButton, Paper, Tooltip, Typography} from "@mui/material";
-import React from 'react';
+import React, {useContext} from 'react';
 
-import {Experience, Reference, Skills, Contacts, Education, QR, Portfolio, Profile} from "design/organisms";
+import {AppSettings} from "App";
+import {LOCALE} from "../../../constants";
+import {Contacts, Education, Experience, Portfolio, Profile, QR, Reference, Skills} from "design/organisms";
 import {useData} from "hooks";
 
 import style from './HomePage.styles'
 
 export function HomePage() {
     const {personal, goals} = useData()
+    const { locale, setLocale } = useContext(AppSettings)
+
+    const handleLocaleChange = () => {
+        if (locale === LOCALE.EN) setLocale(LOCALE.UA);
+        else if (locale === LOCALE.UA) setLocale(LOCALE.EN);
+    }
 
     return (
         <Container>
@@ -23,7 +31,7 @@ export function HomePage() {
                         {goals.join(' / ')}
                     </Typography>
                     <Box sx={style.header.actions}>
-                        <Tooltip title='Translate EN/UA' arrow>
+                        <Tooltip title='Translate EN/UA' arrow onClick={handleLocaleChange}>
                             <IconButton><TranslateIcon/></IconButton>
                         </Tooltip>
                         <Tooltip title='Export as text' arrow>
