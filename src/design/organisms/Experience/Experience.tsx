445c403bf3525e589a292355974f1c24205f0548
kit <kit.mavareik@gmail.com>
2024-09-05 14:52:59 +0300

    Add Experience component with basic layout
    Introduced a new Experience component comprising a title and a container layout. Utilized MUI for styling and created a separate styles file for better modularity.

---
+++ src/design/organisms/Experience/Experience.tsx
@@ -0,0 +1,14 @@
+import {Box, Typography} from "@mui/material";
+
+import style from './Experience.styles'
+
+export const Experience = () => {
+    return (
+        <Box sx={style.container}>
+            <Typography variant='h2'>Work Experience</Typography>
+            <Box>
+
+            </Box>
+        </Box>
+    )
+}
