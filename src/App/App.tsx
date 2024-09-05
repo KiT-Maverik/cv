021cce8947e5631110c7f2c0cf0be1f19ffc0d1e
kit <kit.mavareik@gmail.com>
2024-09-05 16:15:10 +0300

    Add context for locale settings in App component
    Introduced AppSettings context to manage locale state within the application. This allows dynamic changes to the locale and makes the settings easily accessible throughout the component tree.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, {createContext, useMemo, useState} from 'react';
 import {CssBaseline} from "@mui/material";
 
 import {ThemeProvider} from "./utilitary/ThemeProvider/ThemeProvider";
 import {Router} from './utilitary/Router/Router'
+import {LOCALE} from "../constants";
+
+interface AppSettings {
+    locale: LOCALE
+    setLocale: (locale: LOCALE) => void
+}
+export const AppSettings = createContext({} as AppSettings)
 
 export const App = () => {
+    const [locale, setLocale] = useState<LOCALE>(LOCALE.EN)
+
+    const contextValue = useMemo<AppSettings>(
+        () => ({
+            locale,
+            setLocale: (locale: LOCALE) => setLocale(locale),
+        }),
+        [locale],
+    )
+
   return (
-      <ThemeProvider>
-          <CssBaseline/>
-          <Router/>
-      </ThemeProvider>
+      <AppSettings.Provider value={contextValue}>
+          <ThemeProvider>
+              <CssBaseline/>
+              <Router/>
+          </ThemeProvider>
+      </AppSettings.Provider>
   );
 }
