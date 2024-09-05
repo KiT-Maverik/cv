1199494d8de7cd206bc6d0f38035625f3d98e14f
kit <kit.mavareik@gmail.com>
2024-09-05 13:14:34 +0300

    Refactor Education component to streamline imports
    Consolidate and clean up import statements by removing unused icons and hooks, and replacing useDataHook with useData for better clarity and performance.

--- src/design/organisms/Education/Education.tsx
+++ src/design/organisms/Education/Education.tsx
@@ -1,15 +1,12 @@
-import EmailIcon from '@mui/icons-material/MailOutlineRounded';
-import {Box, Button, Chip, Stack, Typography} from "@mui/material";
-import React, {ReactNode, useMemo} from "react";
+import {Box, Stack, Typography} from "@mui/material";
+import React from "react";
 
-import {CONTACTS} from "data";
-import {LinkedinIcon, TelegramIcon} from "design/atoms";
-import {useDataHook} from "hooks/useData.hook";
+import {useData} from "hooks/useData";
 
 import style from './Education.styles'
 
 export const Education = () => {
-    const {education} = useDataHook()
+    const {education} = useData()
 
     return (
         <Box>
