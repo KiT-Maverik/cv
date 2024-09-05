8f733a1b818b4223ff27e402773e4a1f46064749
kit <kit.mavareik@gmail.com>
2024-09-05 19:21:46 +0300

    Add experience and responsibility styles
    Integrated styles for experience periods and responsibility lists in `Experience.styles.ts`. Enhanced `Experience.tsx` to use new styles and dynamically render work experience data from context and hooks.

--- src/design/organisms/Experience/Experience.styles.ts
+++ src/design/organisms/Experience/Experience.styles.ts
@@ -6,11 +6,34 @@ import {commonStyles} from "styles";
 const container: SxProps<Theme> = {
     display: "flex",
     flexDirection: "column",
-}
+} as const
 
 const header: SxProps<Theme> = commonStyles.header
+
+const experience: { container: SxProps<Theme>; period: SxProps<Theme> } = {
+    container: {
+        display: "flex",
+        gap: 3,
+    },
+    period: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    }
+} as const
+
+const responsibility: { list: SxProps<Theme>; item: SxProps<Theme> } = {
+    list: {
+        pl: 5,
+    },
+    item: {
+        listStyleType: 'disc',
+    }
+} as const
 
 export default {
     container,
     header,
+    responsibility,
+    experience,
 }
--- src/design/organisms/Experience/Experience.tsx
+++ src/design/organisms/Experience/Experience.tsx
@@ -1,15 +1,37 @@
 import WorkIcon from '@mui/icons-material/WorkOutlineOutlined';
-import {Box, Typography} from "@mui/material";
+import {Box, Divider, Stack, Typography} from "@mui/material";
+import {useContext} from "react";
+
+import {AppSettings} from "App";
+import {useData} from "hooks";
 
 import style from './Experience.styles'
 
 export const Experience = () => {
+    const {experience} = useData()
+    const { locale } = useContext(AppSettings)
+
+
     return (
         <Box sx={style.container}>
             <Typography variant='h2' sx={style.header}><WorkIcon/>Work Experience</Typography>
-            <Box>
-
-            </Box>
+            {experience.map(({start, end, responsibilities, company, position}) => (
+                    <Box sx={style.experience.container}>
+                        <Stack sx={style.experience.period}>
+                            <Typography>{end}</Typography>
+                            <Typography>-</Typography>
+                            <Typography>{start}</Typography>
+                        </Stack>
+                        <Divider orientation='vertical'/>
+                        <Box>
+                            <Typography>{`${company}, ${position}`}</Typography>
+                            <Box component='ul' sx={style.responsibility.list}>
+                                {responsibilities.map(item => <Typography component='li' sx={style.responsibility.item}>{item[locale]}</Typography>)}
+                            </Box>
+                        </Box>
+                    </Box>
+                ))
+            }
         </Box>
     )
 }
