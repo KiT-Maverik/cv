646256372d6048cf171bf8623443a3cfe8da0b18
kit <kit.mavareik@gmail.com>
2024-09-06 13:30:35 +0300

    Add portfolio styles to Portfolio.styles.ts
    Introduced styles for portfolio container, item, and icon. These styles enhance the layout and appearance of the portfolio section, ensuring it adheres to the existing theme.

--- src/design/organisms/Portfolio/Portfolio.styles.ts
+++ src/design/organisms/Portfolio/Portfolio.styles.ts
@@ -9,7 +9,23 @@ const container: SxProps<Theme> = {
 
 const header: SxProps<Theme> = commonStyles.header
 
+const portfolio: { container: SxProps<Theme>; item: SxProps<Theme>; icon: SxProps<Theme> } = {
+    container:{
+        display: "flex",
+        gap: 3,
+    },
+    item: {
+        flexGrow: 1,
+        fontSize: 32,
+    },
+    icon: {
+        width: 36,
+        aspectRatio: 1,
+    }
+}
+
 export default {
     container,
     header,
+    portfolio,
 }
