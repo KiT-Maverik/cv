4763a0fa2d09e24a9729e998ca1863a1f9924204
kit <kit.mavareik@gmail.com>
2024-09-06 21:24:35 +0300

    Add experience data to raw text output
    Reorder and enhance the raw data output by including experience details, ensuring the information is accurately presented. Adjustments were made to maintain logical grouping of related sections.

--- src/hooks/useRawData.hook.ts
+++ src/hooks/useRawData.hook.ts
@@ -8,11 +8,12 @@ export const useRawData = () => {
         `Name: ${data.personal.firstname} ${data.personal.lastname}`,
         `Goals: ${data.goals.join(' / ')}`,
         `Profile: ${data.personal.profile}`,
-        `Contacts:\n${contactsList.map(item => `${data.contacts[item].link}`).join('\n')}`,
         `Skills: ${data.skills.join(', ')}`,
-        `Education:\n${data.education.map(({facility, specialisation}) => `${facility}, ${specialisation}`).join('\n')}`,
         `Reference:\n${data.reference.map(({email, name, company, position}) => `${name}, ${position} at ${company}. Email: ${email}`).join('\n')}`,
-        `Portfolio:\n${portfolioTypes.map((item) => `${data.portfolio[item].label}: ${data.portfolio[item].link}`).join('\n')}`
+        `Portfolio:\n${portfolioTypes.map((item) => `${data.portfolio[item].label}: ${data.portfolio[item].link}`).join('\n')}`,
+        `Experience:\n${data.experience.map(({company, position, start, end}) => `${position} at ${company}, ${start} - ${end}`).join('\n')}`,
+        `Education:\n${data.education.map(({facility, specialisation}) => `${facility}, ${specialisation}`).join('\n')}`,
+        `Contacts:\n${contactsList.map(item => `${data.contacts[item].link}`).join('\n')}`
     ]
 
     return {rawText: rawData.join('\n\n')}
