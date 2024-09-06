3011ab5356dadfbc2c24717b6199fa85512285f9
kit <kit.mavareik@gmail.com>
2024-09-06 13:05:59 +0300

    Add dynamic portfolio icons and links
    Refactor the Portfolio component to include dynamic icons sourced from the portfolio data. Introduce a useMemo hook for mapping portfolio types to their respective icons, and render portfolio items with appropriate links and labels.

--- src/design/organisms/Portfolio/Portfolio.tsx
+++ src/design/organisms/Portfolio/Portfolio.tsx
@@ -1,15 +1,36 @@
 import PortfolioIcon from '@mui/icons-material/DiamondOutlined';
 import {Box, Typography} from "@mui/material";
 
+import {PORTFOLIO, portfolioTypes} from "data";
+import {GithubIcon, AIIcon, ICodeThisIcon, LeetCodeIcon} from "design/atoms";
 import style from './Portfolio.styles'
+import {useData} from "hooks";
+import {useMemo} from "react";
 
 export const Portfolio = () => {
+    const {portfolio} = useData()
+
+    const icons = useMemo(() => ({
+        [PORTFOLIO.AI]: <AIIcon/>,
+        [PORTFOLIO.GITHUB]: <GithubIcon/>,
+        [PORTFOLIO.I_CODE_THIS]: <ICodeThisIcon/>,
+        [PORTFOLIO.LEET_CODE]: <LeetCodeIcon/>,
+    }), []);
+
     return (
         <Box sx={style.container}>
             <Typography variant='h2' sx={style.header}><PortfolioIcon/>Portfolio</Typography>
-            <Box>
-
+            {portfolioTypes.map(item => (
+                <Box>
+                <Box component='a' href={portfolio[item].link} sx={{display: 'flex', gap: 3, alignItems: 'center'}}>
+                    <Box sx={{width: 36, aspectRatio: 1}}>
+                        {icons[item]}
+                    </Box>
+                    <Typography variant='h2'>{portfolio[item].label}</Typography>
+                </Box>
             </Box>
+            ))
+            }
         </Box>
     )
 }
