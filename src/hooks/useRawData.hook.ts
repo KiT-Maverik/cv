4aa2e8e5cab67abeae94ef6bfe0c3df05c5ddfd2
kit <kit.mavareik@gmail.com>
2024-09-06 20:31:43 +0300

    Add profile and portfolio information to raw data
    Include the 'profile' field in the raw data generation and introduce portfolio details using `portfolioTypes` from data. Adjust raw data formatting for improved readability.

--- src/hooks/useRawData.hook.ts
+++ src/hooks/useRawData.hook.ts
@@ -1,4 +1,4 @@
-import {contactsList} from "data";
+import {contactsList, portfolioTypes} from "data";
 import {useData} from "hooks";
 
 export const useRawData = () => {
@@ -7,10 +7,12 @@ export const useRawData = () => {
     const rawData = [
         `Name: ${data.personal.firstname} ${data.personal.lastname}`,
         `Goals: ${data.goals.join(' / ')}`,
-        `Contacts:\n${contactsList.map(item => `- ${data.contacts[item].link}`).join('\n')}`,
+        `Profile: ${data.personal.profile}`,
+        `Contacts:\n${contactsList.map(item => `${data.contacts[item].link}`).join('\n')}`,
         `Skills: ${data.skills.join(', ')}`,
-        `Education:\n${data.education.map(({facility, specialisation}) => `- ${facility}, ${specialisation}`).join('\n')}`,
-        `Reference:\n${data.reference.map(({email, name, company, position}) => `- ${name}, ${position} at ${company}. Email: ${email}`).join('\n')}`,
+        `Education:\n${data.education.map(({facility, specialisation}) => `${facility}, ${specialisation}`).join('\n')}`,
+        `Reference:\n${data.reference.map(({email, name, company, position}) => `${name}, ${position} at ${company}. Email: ${email}`).join('\n')}`,
+        `Portfolio:\n${portfolioTypes.map((item) => `${data.portfolio[item].label}: ${data.portfolio[item].link}`).join('\n')}`
     ]
 
     return {rawText: rawData.join('\n\n')}
