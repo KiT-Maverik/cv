60fecaa798f72a21c1132a64cceb6148bc0d7f1c
kit <kit.mavareik@gmail.com>
2024-09-06 13:36:39 +0300

    Add Roboto font to the project
    Import various Roboto font weights in App.tsx and update package.json to include @fontsource/roboto dependency. This enhances the application's typography by implementing the Roboto typeface.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,8 @@
 import React, {createContext, useMemo, useState} from 'react';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import {CssBaseline} from "@mui/material";
 
 import {ThemeProvider} from "./utilitary/ThemeProvider/ThemeProvider";
