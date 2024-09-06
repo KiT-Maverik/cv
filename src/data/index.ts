bf990f3ac2602f4891d41df1254e01ee34266ee6
kit <kit.mavareik@gmail.com>
2024-09-06 13:05:37 +0300

    Add portfolio data export
    This commit introduces a new `portfolio` export to the data module. It defines an enum for portfolio categories and structures links associated with each category. This allows for easy referencing and utilization of portfolio data across the application.

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,6 +1,7 @@
 export * from './education'
 export * from './experience'
 export * from './personal'
+export * from './portfolio'
 export * from './reference'
 export * from './contacts'
 export * from './skills'
---
+++ src/data/portfolio.ts
@@ -0,0 +1,32 @@
+export enum  PORTFOLIO {
+    AI = 'AI',
+    GITHUB= 'GitHub',
+    I_CODE_THIS = 'I code this',
+    LEET_CODE = 'Leet Code',
+}
+
+export const portfolioTypes = Object.values(PORTFOLIO)
+
+interface Portfolio {
+    link: string
+    label: string
+}
+
+export const portfolio: {[key in PORTFOLIO]: Portfolio} = {
+    [PORTFOLIO.AI]: {
+        label: 'AI Prompts',
+        link: 'https://gist.github.com/KiT-Maverik'
+    },
+    [PORTFOLIO.GITHUB]: {
+        label: 'GitHub',
+        link: 'https://github.com/KiT-Maverik'
+    },
+    [PORTFOLIO.I_CODE_THIS]: {
+        label: 'I Code This',
+        link: 'https://icodethis.com/KiT_Gart'
+    },
+    [PORTFOLIO.LEET_CODE]: {
+        label: 'Leet Code',
+        link: 'https://leetcode.com/u/KiT_Koval/'
+    },
+}
