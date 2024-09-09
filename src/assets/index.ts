db865b34aa5d24d4bfc6ac01b43458c733e1ce90
kit <kit.mavareik@gmail.com>
2024-09-09 09:45:00 +0300

    Move and re-export icons from design to assets
    Moved icon files from 'src/design/atoms/icons' to 'src/assets/icons' for better file organization. A new 'src/assets/index.ts' file is created to re-export these icons and a QR code for easier import elsewhere in the project. This change improves the maintainability and structure of the asset files.

---
+++ src/assets/index.ts
@@ -0,0 +1,8 @@
+// Icons
+export { ReactComponent as AIIcon } from "./icons/ai.svg";
+export { ReactComponent as GithubIcon } from "./icons/github.svg";
+export { ReactComponent as ICodeThisIcon } from "./icons/icodethis.svg";
+export { ReactComponent as LeetCodeIcon } from "./icons/leetcode.svg";
+
+// QR
+export { ReactComponent as QRcode } from "./qr-video-presentation.svg";
