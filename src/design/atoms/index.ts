f53f48f123faeba8f3ab907d272cc2eb979385cc
kit <kit.mavareik@gmail.com>
2024-09-09 09:45:18 +0300

    Remove unused icon exports
    Deleted the `src/design/atoms/index.ts` file, which contained exports for four SVG icons that are no longer being used in the codebase. This change helps to clean up the project by removing dead code, thereby reducing potential confusion and improving maintainability. No functionality is affected since these icons were not referenced anywhere.

--- src/design/atoms/index.ts
+++
@@ -1,4 +0,0 @@
-export { ReactComponent as AIIcon } from "./icons/ai.svg";
-export { ReactComponent as GithubIcon } from "./icons/github.svg";
-export { ReactComponent as ICodeThisIcon } from "./icons/icodethis.svg";
-export { ReactComponent as LeetCodeIcon } from "./icons/leetcode.svg";
