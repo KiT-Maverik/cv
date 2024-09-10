5bcc8c9687ef6b46c2bb7054d4f72d9c0bbaefcb
kit <kit.mavareik@gmail.com>
2024-09-10 16:44:36 +0300

    Remove hover-based showCopyButton state logic
    Eliminated the `showCopyButton` state and associated hover logic from the Contacts component. The copy button is now always displayed, simplifying the UI and improving consistency. This change reduces unnecessary state management and potential flickering issues caused by hover events.

--- src/design/organisms/Contacts/Contacts.tsx
+++ src/design/organisms/Contacts/Contacts.tsx
@@ -3,15 +3,15 @@ import TelegramIcon from "@mui/icons-material/SendRounded";
 import EmailIcon from "@mui/icons-material/MailOutlineRounded";
 import LinkedinIcon from "@mui/icons-material/AccountBoxRounded";
 import WebsiteIcon from "@mui/icons-material/LanguageRounded";
-import { Box, Button, Fade, IconButton, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import React, { ReactNode, useMemo, useState } from "react";
 
 import { CONTACTS } from "data";
 import { useCopyToClipboard, useData, useLocale } from "hooks";
 
 import style from "./Contacts.styles";
+
 export const Contacts = () => {
-  const [showCopyButton, setShowCopyButton] = useState(false);
   const { contacts } = useData();
   const { sections } = useLocale();
   const { copy } = useCopyToClipboard();
@@ -29,32 +29,24 @@ export const Contacts = () => {
       <Typography variant="h2">{sections.contacts}</Typography>
       {Object.values(CONTACTS).map((item) => (
         <>
-          <Box
-            key={item}
-            sx={style.action.web}
-            onMouseEnter={() => setShowCopyButton(true)}
-            onMouseLeave={() => setShowCopyButton(false)}
-          >
+          <Box key={item} sx={style.action.web}>
             <Button
               color="inherit"
               component="a"
               href={contacts[item].link}
-              target="_blank"
               fullWidth
               startIcon={icon[item]}
             >
               {contacts[item].label}
             </Button>
-            <Fade in={showCopyButton}>
-              <Button
-                key={item}
-                color="inherit"
-                sx={{ minWidth: 0 }}
-                onClick={() => copy(contacts[item].link)}
-              >
-                <CopyIcon />
-              </Button>
-            </Fade>
+            <Button
+              key={item}
+              color="inherit"
+              sx={{ minWidth: 0 }}
+              onClick={() => copy(contacts[item].link)}
+            >
+              <CopyIcon color="disabled" sx={{ fontSize: 16 }} />
+            </Button>
           </Box>
         </>
       ))}
